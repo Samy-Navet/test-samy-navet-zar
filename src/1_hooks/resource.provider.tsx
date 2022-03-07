87d94bb1f3b968f89842c3402ec47ca4eb1a9f48
Samy-Navet <samy.navet@eemi.com>
2022-03-07 18:16:10 +0100

    TODO 2 & 3

--- src/1_hooks/resource.provider.tsx
+++ src/1_hooks/resource.provider.tsx
@@ -21,7 +21,6 @@ export const ResourceProvider: React.FC = ({
     const getChampionImage = useCallback((championId: Client.ChampionId) => {
         // TODO: This function should return the champion image url based on the passed championId
         // e.g. for Annie it's the following: https://cdn.zargg.workers.dev/champion/1.png
-        if (!championId) throw new Error('missing championID');
         return `https://cdn.zargg.workers.dev/champion/${championId}.png`
     }, []);
 
--- src/2_components/1_draft-summoner-profile/draft-summoner-profile.tsx
+++ src/2_components/1_draft-summoner-profile/draft-summoner-profile.tsx
@@ -7,7 +7,20 @@ import { Client, isChampionIdValid, isRoleValid } from '../../common/league';
 // https://v4.mui.com/styles/api/#examples-2
 const useStyles = makeStyles(theme => ({
     root: {
-
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+    },
+    column: {
+        marginRight: 'auto',
+        alignSelf: 'flex-start',
+    },
+    image: {
+        display: 'block',
+        height: '70px',
+        alignSelf: 'center',
+        marginRight: '10px'
+        //flexGrow: 1
     }
 }));
 
@@ -43,6 +56,8 @@ export interface DraftSummonerProfileProps {
 // - If winrate is >= 50, it's positive and displayed in our primary color
 // - If gamesPlayed is > 0, the profile contains data
 
+const noData = 'no data';
+
 export const DraftSummonerProfile: React.FC<DraftSummonerProfileProps> = ({
     profile: {
         summonerName,
@@ -71,15 +86,74 @@ export const DraftSummonerProfile: React.FC<DraftSummonerProfileProps> = ({
     const hasRole = isRoleValid(role);
     const hasChampion = isChampionIdValid(championId);
 
+    const champion = hasChampion ? getChampionName(championId) : noData;
+    const championImage = getChampionImage(hasChampion ? championId : 0);
+
     return (
-        <Card elevation='1' p={1}>
-            <Typography
-                variant='textMain' paragraph
-                color='textSecondary'
-                mt={2.5} mb={2.5}
-            >
-                Not implemented.
-            </Typography>
+        <Card elevation='1' style={{ borderRadius: '2px', margin: '8px auto', height: '80px', padding: '5px' }} p={1}>
+            <div className={classes.root}>
+                <img src={championImage} alt="" className={classes.image} style={!hasChampion ? { border: 'solid white 2px' } : {}} />
+                <div className={classes.column}>
+                    <Typography
+                        color='textSecondary'
+                        mt={0.5} mb={1}
+                    >
+                        {summonerName}
+                    </Typography>
+                    {gamesPlayed ? (
+                        <div>
+                            <Typography color='primary' mt={1} mb={1}>
+                                {`${winrate.toFixed(1)}% wr`}
+                            </Typography>
+                            <Typography color='textSecondary' variant="textSmall" mt={1} mb={1}>
+                                {`${capitalize(tier)} ${division}`}
+                            </Typography>
+                            <Typography color='textTertiary' variant="textExtraSmall" mt={1} mb={1}>
+                                {`${gamesPlayed} games`}
+                            </Typography>
+                        </div>
+                    ) : NoData}
+                </div>
+                {hasRole && !hasChampion ? (
+                    <div className={classes.column}>
+                        <Typography color='textSecondary' mt={0.5} mb={1}>
+                            {`as ${capitalize(role)}`}
+                        </Typography>
+                        {gamesPlayed ? <DetailsList winrate={roleProfile.winrate} kda={roleProfile.kda} gamesPlayed={roleProfile.gamesPlayed} /> : NoData}
+                    </ div>
+                ) : null}
+                {hasChampion ? (
+                    <div className={classes.column}>
+                        <Typography color='textSecondary' mt={0.5} mb={1}>
+                            {`on ${champion}`}
+                        </Typography>
+                        {gamesPlayed ? <DetailsList winrate={championProfile.winrate} kda={championProfile.kda} gamesPlayed={championProfile.gamesPlayed} /> : NoData}
+                    </div>
+                ) : null}
+            </div>
         </Card>
     );
 }
+
+
+const DetailsList = ({ winrate, kda, gamesPlayed }: { winrate: number; kda: number; gamesPlayed: number }) => (
+    <>
+        <Typography color={winrate > 50 ? 'primary' : 'textPrimary'} mb={1}>
+            {`${winrate.toFixed(1)}% wr`}
+        </Typography>
+        <Typography color={'textPrimary'} variant="textSmall" mb={1}>
+            {`${kda.toFixed(1)} kda`}
+        </Typography>
+        <Typography color='textTertiary' variant="textExtraSmall" mb={1}>
+            {`${gamesPlayed} games`}
+        </Typography>
+    </>
+)
+
+const NoData = (
+    <Typography color='textSecondary' mb={1}>
+        no data
+    </Typography>
+)
+
+const capitalize = (s: string): string => s && s[0].toUpperCase() + s.slice(1).toLowerCase();
